b44908cf73a0c46dbeb68a7f8dd99a31f87dc8a5
Guote <guotefac@gmail.com>
2021-03-16 23:43:36 +0100

    Call super.nextRound() instead of copying the method

--- src/module/combat.ts
+++ src/module/combat.ts
@@ -1,23 +1,9 @@
 // @ts-nocheck
 export default class abfCombat extends Combat {
-    async nextRound() {
-       // Reset initiative for everyone when going to the next round
-       this.resetAll()
+  async nextRound() {
+    // Reset initiative for everyone when going to the next round
+    this.resetAll();
 
-       super.nextRound();
-        let turn = 0;
-        if (this.settings.skipDefeated) {
-            turn = this.turns.findIndex(t => {
-                return !(t.defeated ||
-                    t.actor?.effects.find(e => e.getFlag("core", "statusId") === CONFIG.Combat.defeatedStatusId));
-            });
-            if (turn === -1) {
-                ui.notifications.warn(game.i18n.localize("COMBAT.NoneRemaining"));
-                turn = 0;
-            }
-        }
-        let advanceTime = Math.max(this.turns.length - this.data.turn, 1) * CONFIG.time.turnTime;
-        advanceTime += CONFIG.time.roundTime;
-        return this.update({ round: this.round + 1, turn: turn }, { advanceTime });
-    }
+    super.nextRound();
+  }
 }
