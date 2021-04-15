4e9f144ae016eb26bb6778ba6cf825ba16369d0b
Guote <guotefac@gmail.com>
2021-04-15 19:23:22 +0200

    Link external html files for each tab to main html sheet

--- src/module/abfActorSheet.ts
+++ src/module/abfActorSheet.ts
@@ -1,11 +1,17 @@
 export default class abfActorSheet extends ActorSheet {
-
-    static get defaultOptions() {
-      return mergeObject(super.defaultOptions, {
-        classes: ["abf", "sheet", "actor"], 
-        template: "systems/animabf/templates/actor-sheet.html",
-        width: 600,
-        height: 700,
-      });
-    } 
-  };
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ["abf", "sheet", "actor"],
+      template: "systems/animabf/templates/actor-sheet.html",
+      width: 600,
+      height: 700,
+      tabs: [
+        {
+          navSelector: ".sheet-tabs",
+          contentSelector: ".sheet-body",
+          initial: "main",
+        },
+      ],
+    });
+  }
+}
--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -1,7 +1,15 @@
-export const preloadTemplates = async function() {
-	const templatePaths = [
-		// Add paths to "systems/AnimaBeyondFoundry/templates"
-	];
+export const preloadTemplates = async function () {
+  const templatePaths = [
+    // Add paths to "systems/AnimaBeyondFoundry/templates"
 
-	return loadTemplates(templatePaths);
-}
+    // Actor sheet parts
+    "systems/animabf/templates/parts/combat.html",
+    "systems/animabf/templates/parts/domine.html",
+    "systems/animabf/templates/parts/elan.html",
+    "systems/animabf/templates/parts/main.html",
+    "systems/animabf/templates/parts/mystic.html",
+    "systems/animabf/templates/parts/secondaries.html",
+  ];
+
+  return loadTemplates(templatePaths);
+};
