9fd922b23c14e3a0fa5e7177d1bc7b00a0a8f492
Guote <guotefac@gmail.com>
2021-04-27 17:01:34 +0200

    Add language files and start localization. Added a handlebars helper to concatenate strings before lozalization.

--- src/animabf.ts
+++ src/animabf.ts
@@ -41,6 +41,16 @@ Hooks.once("init", async function () {
   // Preload Handlebars templates
   await preloadTemplates();
 
+  Handlebars.registerHelper("concat", function () {
+    var outStr = "";
+    for (var arg in arguments) {
+      if (typeof arguments[arg] != "object") {
+        outStr += arguments[arg];
+      }
+    }
+    return outStr;
+  });
+
   // Register custom sheets (if any)
   Actors.unregisterSheet("core", ActorSheet);
   Actors.registerSheet("abf", abfActorSheet, { makeDefault: true });
