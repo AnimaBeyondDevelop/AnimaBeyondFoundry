a7cc9ba3a43b2797474d4aa330282b5fe09f93fe
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2021-05-26 16:12:45 +0100

    changes: add eslint rules

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['src/**/*.ts'],
+      extends: ['plugin:@typescript-eslint/recommended', 'airbnb-typescript/base'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname
+      },
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['off'],
+        '@typescript-eslint/explicit-module-boundary-types': ['off'],
+        'import/prefer-default-export': ['off'],
+        'no-shadow': 'off',
+        '@typescript-eslint/no-shadow': ['error']
+      }
+    },
+    {
+      files: ['src/**/*.spec.ts', '**/__test__/**'],
+      extends: ['plugin:@typescript-eslint/recommended', 'airbnb-typescript/base'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.specs.json',
+        tsconfigRootDir: __dirname
+      },
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': ['off'],
+        '@typescript-eslint/explicit-module-boundary-types': ['off']
+      }
+    }
+  ]
+};
