67261731c2d260be6bbb2cc54a4595bf758888a2
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2021-06-19 11:42:47 +0100

    fix: typos and remove unused lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,26 +22,6 @@ module.exports = {
         'no-shadow': 'off',
         '@typescript-eslint/no-shadow': ['error']
       }
-    },
-    {
-      files: ['src/**/*.spec.ts', '**/__test__/**'],
-      extends: ['plugin:@typescript-eslint/recommended', 'airbnb-typescript/base'],
-      parser: '@typescript-eslint/parser',
-      parserOptions: {
-        project: './tsconfig.specs.json',
-        tsconfigRootDir: __dirname
-      },
-      rules: {
-        'no-restricted-syntax': 'off',
-        '@typescript-eslint/comma-dangle': ['off'],
-        'import/no-cycle': ['off'],
-        '@typescript-eslint/lines-between-class-members': ['off'],
-        'operator-linebreak': ['off'],
-        'arrow-parens': ['off'],
-        'no-underscore-dangle': ['off'],
-        '@typescript-eslint/explicit-function-return-type': ['off'],
-        '@typescript-eslint/explicit-module-boundary-types': ['off']
-      }
     }
   ]
 };
