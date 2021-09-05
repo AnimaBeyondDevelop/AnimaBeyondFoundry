1b8413c49e5c5a750bf6f9274a614c0832653441
Jesé Romero Arbelo <jeseromeroarbelo@gmail.com>
2021-09-05 15:55:33 +0100

    hotfix: control roll

--- src/module/rolls/ABFControlRoll/ABFControlRoll.ts
+++ src/module/rolls/ABFControlRoll/ABFControlRoll.ts
@@ -5,14 +5,14 @@ export default class ABFControlRoll extends ABFRoll {
   success = false;
 
   public evaluate(): ABFFoundryRoll {
-    let penalty = Math.min(0, Math.floor(this.foundryRoll.data.general.modifiers.allActions.final.value / 20));
+    let penalty = Math.max(0, Math.floor(-this.foundryRoll.data.general.modifiers.allActions.base.value / 20));
 
     if (this.foundryRoll.total === 10) {
       this.success = true;
-      penalty += 2;
+      penalty -= 2;
     }
 
-    this.foundryRoll.recalculateTotal(penalty);
+    this.foundryRoll.recalculateTotal(-penalty);
 
     return this.foundryRoll;
   }
