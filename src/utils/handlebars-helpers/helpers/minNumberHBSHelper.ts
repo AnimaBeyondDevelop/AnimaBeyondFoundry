660e3ef8a2389809310b17027c119c4837a62293
daviddpg <daviddelprado94@gmail.com>
2021-09-26 13:23:04 +0200

    cleanup handlebars helper

--- src/utils/handlebars-helpers/helpers/minNumberHBSHelper.ts
+++ src/utils/handlebars-helpers/helpers/minNumberHBSHelper.ts
@@ -1,12 +1,6 @@
 import { HandlebarsHelper } from '../registerHelpers';
-import { Log } from '../../Log';
 
 export const minNumberHBSHelper: HandlebarsHelper<string> = {
   name: 'minNumber',
-  fn: (first: number, second: number) => {
-    const result = Math.min(first, second);
-    // eslint-disable-next-line no-console
-    Log.log(result);
-    return result.toString();
-  }
+  fn: (first: number, second: number) => Math.min(first, second).toString()
 };
