588b6e5ac8e49b6c14b38d8225c413f768f36bb7
daviddpg <daviddelprado94@gmail.com>
2021-09-27 00:02:37 +0200

    Revert min number helper type back to number

--- src/utils/handlebars-helpers/helpers/minNumberHBSHelper.ts
+++ src/utils/handlebars-helpers/helpers/minNumberHBSHelper.ts
@@ -1,6 +1,6 @@
 import { HandlebarsHelper } from '../registerHelpers';
 
-export const minNumberHBSHelper: HandlebarsHelper<string> = {
+export const minNumberHBSHelper: HandlebarsHelper<number> = {
   name: 'minNumber',
-  fn: (first: number, second: number) => Math.min(first, second).toString()
+  fn: (first: number, second: number) => Math.min(first, second)
 };
