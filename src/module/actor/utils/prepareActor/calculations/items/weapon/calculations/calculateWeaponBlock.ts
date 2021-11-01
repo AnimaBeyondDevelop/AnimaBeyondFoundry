bf6ad5601e38130167ed778f98262a1d38223351
daviddpg <daviddelprado94@gmail.com>
2021-11-01 13:52:40 +0100

    Change shield bonus to weapon block instead of actor block

--- src/module/actor/utils/prepareActor/calculations/items/weapon/calculations/calculateWeaponBlock.ts
+++ src/module/actor/utils/prepareActor/calculations/items/weapon/calculations/calculateWeaponBlock.ts
@@ -2,10 +2,12 @@ import { WeaponDataSource } from '../../../../../../../types/combat/WeaponItemCo
 import { ABFActorDataSourceData } from '../../../../../../../types/Actor';
 import { getWeaponKnowledgePenalty } from '../util/getWeaponKnowledgePenalty';
 import { calculateStrengthRequiredPenalty } from '../util/calculateStrengthRequiredPenalty';
+import { calculateShieldBlockBonus } from '../../../actor/combat/calculations/calculateShieldBlockBonus';
 
 export const calculateWeaponBlock = (weapon: WeaponDataSource, data: ABFActorDataSourceData) =>
   data.combat.block.final.value +
   weapon.data.block.special.value +
   weapon.data.quality.value +
+  (weapon.data.isShield.value && weapon.data.equipped.value ? calculateShieldBlockBonus(weapon) : 0) +
   getWeaponKnowledgePenalty(weapon) +
   calculateStrengthRequiredPenalty(weapon, data);
