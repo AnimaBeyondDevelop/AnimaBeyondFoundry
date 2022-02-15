66a8208725910ba19b4eb34c275cb62b5faff278
A. Balmaseda <aitor.5502.b@gmail.com>
2022-02-15 23:02:22 +0100

    Fixing indentation of getSelectedToken after modifications on pull request #111

--- src/module/combat/websocket/ws-combat/util/getSelectedToken.ts
+++ src/module/combat/websocket/ws-combat/util/getSelectedToken.ts
@@ -1,14 +1,14 @@
 import { ABFDialogs } from "../../../../dialogs/ABFDialogs";
 
 export function getSelectedToken(game: Game): TokenDocument {
-const selectedTokens = game.canvas.tokens?.controlled ?? [];
+  const selectedTokens = game.canvas.tokens?.controlled ?? [];
 
-if (selectedTokens.length !== 1) {
+  if (selectedTokens.length !== 1) {
     const msg = game.i18n.localize(selectedTokens.length > 0 ? 'macros.combat.dialog.error.multipleSelectedToken.title' : 'macros.combat.dialog.error.noSelectedToken.title');
 
     ABFDialogs.prompt(msg);
     throw new Error(msg);
-}
+  }
 
-return selectedTokens[0].document;
+  return selectedTokens[0].document;
 }
