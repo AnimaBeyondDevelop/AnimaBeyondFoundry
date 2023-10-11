ed2a5c250b771fe879d8ba52a913587857662b71
paiva92 <paivaalfonso@gmail.com>
2023-10-11 00:53:55 -0300

    fix-resetDefensesCounterHook

--- src/module/utils/hooks-scripts/resetDefensesCounterHook.js
+++ src/module/utils/hooks-scripts/resetDefensesCounterHook.js
@@ -1,6 +1,6 @@
 export const resetDefensesCounterHook = () => {
   const gameCombatants = game.combat.combatants.map(c => c.token)
-  const combatantsAcc = gameCombatants.filter(i => i.actor.flags.world[i.actor._id].defensesCounter.accumulated !== 0)
+  const combatantsAcc = gameCombatants.filter(i => i.actor.flags?.world[i.actor._id]?.defensesCounter?.accumulated !== 0);
   if (combatantsAcc.length !== 0) {
     for (let combantantAcc of combatantsAcc) {
       combantantAcc.actor.resetDefensesCounter()
