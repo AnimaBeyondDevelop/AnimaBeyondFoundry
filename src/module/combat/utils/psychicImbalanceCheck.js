7027431720462d03dca13ba59f7769dcdc3f6770
paiva92 <paivaalfonso@gmail.com>
2023-10-19 06:14:47 -0300

    fix-psychicImbalanceCheck

--- src/module/combat/utils/psychicImbalanceCheck.js
+++ src/module/combat/utils/psychicImbalanceCheck.js
@@ -3,7 +3,7 @@ export const psychicImbalanceCheck = (actor, power) => {
     const powerDiscipline = power?.system.discipline.value
     const regExpMental = new RegExp("Valentía");
     let imbalance = 0
-    if (psychicDisciplines.find(i => i.name == powerDiscipline).system.imbalance.value) {imbalance++}
+    if (psychicDisciplines.find(i => i.name == powerDiscipline)?.system.imbalance.value) {imbalance++}
     if (power?.system.combatType.value == 'attack' && regExpMental.test(mentalPatterns?.map(i => i.name))) {imbalance++}
     return imbalance
     };
