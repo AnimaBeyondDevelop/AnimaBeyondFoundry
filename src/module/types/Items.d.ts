b5bd40af8bff95b1a045a6b885e35d897150586b
Daraguz <143577521+paiva92@users.noreply.github.com>
2023-10-31 17:13:19 -0300

    Update src/module/types/Items.d.ts
    Co-authored-by: Aitor Balmaseda <32964371+Eryx5502@users.noreply.github.com>

--- src/module/types/Items.d.ts
+++ src/module/types/Items.d.ts
@@ -9,7 +9,7 @@ import { INITIAL_ACT_VIA_DATA } from './mystic/ActViaItemConfig';
 import { INITIAL_INNATE_MAGIC_VIA_DATA } from './mystic/InnateMagicViaItemConfig';
 import { INITIAL_MYSTIC_SPELL_DATA } from './mystic/SpellItemConfig';
 import { INITIAL_MYSTIC_SHIELD_DATA } from './mystic/MysticShieldItemConfig';
-import { INITIAL_PSYCHIC_DISCIPLINE_DATA } from './mystic/PsychicDisciplineItemConfig';
+import { INITIAL_PSYCHIC_DISCIPLINE_DATA } from './psychic/PsychicDisciplineItemConfig';
 import { INITIAL_PSYCHIC_POWER_DATA } from './psychic/PsychicPowerItemConfig';
 import { INITIAL_PSYCHIC_SHIELD_DATA } from './psychic/PsychicShieldItemConfig';
 import { INITIAL_TECHNIQUE_DATA } from './domine/TechniqueItemConfig';
