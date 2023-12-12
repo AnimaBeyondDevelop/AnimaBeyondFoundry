30314e2832c5a06bb8e9b142eb73457b1ca50ae2
A. Balmaseda <aitor.5502.b@gmail.com>
2023-12-12 14:39:58 +0100

    First version of some elementary ui components (customSelect, titledInput) to replace handlebars counterparts

---
+++ src/svelte/ui/customSelect.svelte
@@ -0,0 +1,20 @@
+<script>
+  export let vertical = false;
+  export let cls = '';
+  /** @type {string | undefined} */
+  export let title = undefined;
+  /** @type {string | undefined} */
+  export let name = undefined;
+  export let disabled = false;
+  export let value;
+</script>
+
+<div class="common-titled-input {cls}" class:vertical>
+  {#if title}
+    <p class="label">{title}</p>
+  {/if}
+
+  <select class="input" {name} {disabled} bind:value>
+    <slot />
+  </select>
+</div>
