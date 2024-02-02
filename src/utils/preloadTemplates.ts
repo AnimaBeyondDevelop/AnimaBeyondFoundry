ae462724c0672d95f9298b236676ec501f80af4a
daraguz <paivaalfonso@gmail.com>
2024-02-02 05:34:14 -0300

    replace visuals-effects for status

--- src/utils/preloadTemplates.ts
+++ src/utils/preloadTemplates.ts
@@ -140,8 +140,9 @@ export const preloadTemplates = () => {
     'systems/animabf/templates/actor/parts/settings/parts/automationOptions.hbs',
     'systems/animabf/templates/actor/parts/settings/parts/advancedSettings.hbs',
     'systems/animabf/templates/actor/parts/settings/parts/advancedCharacteristics.hbs',
-    'systems/animabf/templates/actor/parts/effects/effects.hbs',
-    'systems/animabf/templates/actor/parts/effects/parts/effects-list.hbs'
+    'systems/animabf/templates/actor/parts/status/status.hbs',
+    'systems/animabf/templates/actor/parts/status/parts/effects-list.hbs',
+    'systems/animabf/templates/actor/parts/status/parts/pain-modifiers.hbs'
   ];
 
   return loadTemplates(templatePaths);
