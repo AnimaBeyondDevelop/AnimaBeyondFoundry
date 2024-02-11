29b525e7208390dd2323989ee3bd445101f93852
A. Balmaseda <aitor.5502.b@gmail.com>
2024-02-11 01:17:36 +0100

    Fix: outdated references in mutateCombatData()

--- src/module/actor/utils/prepareActor/calculations/actor/combat/mutateCombatData.js
+++ src/module/actor/utils/prepareActor/calculations/actor/combat/mutateCombatData.js
@@ -2,15 +2,15 @@ export const mutateCombatData = data => {
   data.combat.attack.final.value =
     data.combat.attack.base.value +
     data.general.modifiers.allActions.final.value +
-    data.general.modifiers.physicalActions.value;
+    data.general.modifiers.physicalActions.final.value;
 
   data.combat.block.final.value =
     data.combat.block.base.value +
     data.general.modifiers.allActions.final.value +
-    data.general.modifiers.physicalActions.value;
+    data.general.modifiers.physicalActions.final.value;
 
   data.combat.dodge.final.value =
     data.combat.dodge.base.value +
     data.general.modifiers.allActions.final.value +
-    data.general.modifiers.physicalActions.value;
+    data.general.modifiers.physicalActions.final.value;
 };
