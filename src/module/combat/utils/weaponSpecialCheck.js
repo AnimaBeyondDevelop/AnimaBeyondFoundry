f6868bc33e3fd9e2fd06c64dccceb4138217ddb3
daraguz <paivaalfonso@gmail.com>
2024-02-20 09:02:06 -0300

    fix - weapon Special

--- src/module/combat/utils/weaponSpecialCheck.js
+++ src/module/combat/utils/weaponSpecialCheck.js
@@ -1,5 +1,5 @@
 export const weaponSpecialCheck = (weapon) => {
     const specialEffect = weapon?.system.special.value;
-    if (/Presa/.test(specialEffect)) { return parseInt(specialEffect.match(/Presa \(Fuerza \d+\)/)[0].match(/\d+/)[0], 10)?? 0}
+    if (/Presa/.test(specialEffect)) { return parseInt(specialEffect.match(/Presa .*\d+\)/)[0].match(/\d+/)[0], 10)?? 0}
     else { return }
 };
