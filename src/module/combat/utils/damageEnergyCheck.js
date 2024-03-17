a816da774bf3f8aeb37809545186d96c6767dd0a
daraguz <paivaalfonso@gmail.com>
2024-03-17 18:32:14 -0300

    fix- damageEnergyCheck

--- src/module/combat/utils/damageEnergyCheck.js
+++ src/module/combat/utils/damageEnergyCheck.js
@@ -1,12 +1,12 @@
 import { ABFItems } from '../../items/ABFItems';
 export const damageEnergyCheck = (item, dificulty) => {
-  if (item.type === ABFItems.WEAPON) {
+  if (item?.type === ABFItems.WEAPON) {
     return item.system.damageEnergy
   };
-  if (item.type === ABFItems.SPELL) {
+  if (item?.type === ABFItems.SPELL) {
     return item.system.damageEnergy
   };
-  if (item.type === ABFItems.PSYCHIC_POWER) {
+  if (item?.type === ABFItems.PSYCHIC_POWER) {
     return item.system.damageEnergy.value >= dificulty
   };
   return false
