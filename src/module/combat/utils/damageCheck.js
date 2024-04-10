facbca7f1afd1af3b6090a6f0dd6225f9b1131f5
daraguz <paivaalfonso@gmail.com>
2024-04-10 07:27:42 -0300

    update - damageCheck

--- src/module/combat/utils/damageCheck.js
+++ src/module/combat/utils/damageCheck.js
@@ -1,7 +1,7 @@
 export const damageCheck = effect => {
   effect = effect.replace('.', '');
-  if (/Daño[^t\d]*\d+/i.test(effect)) {
-    return parseInt(effect.match(/Daño[^t\d]*\d+/i)[0].match(/\d+/)[0], 10) ?? 0;
+  if (/Daño *\d+/i.test(effect)) {
+    return parseInt(effect.match(/Daño *\d+/i)[0].match(/\d+/)[0], 10) ?? 0;
   } else {
     return 0;
   }
