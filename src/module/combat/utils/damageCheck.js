fa616deaf4eadb01d3d4dc02b475079296649e0b
daraguz <paivaalfonso@gmail.com>
2024-04-10 06:42:32 -0300

    fix - regular expression in damageCheck to evaluate a dot in the text description

--- src/module/combat/utils/damageCheck.js
+++ src/module/combat/utils/damageCheck.js
@@ -1,4 +1,5 @@
 export const damageCheck = effect => {
+  effect = effect.replace('.', '');
   if (/Daño[^t\d]*\d+/i.test(effect)) {
     return parseInt(effect.match(/Daño[^t\d]*\d+/i)[0].match(/\d+/)[0], 10) ?? 0;
   } else {
