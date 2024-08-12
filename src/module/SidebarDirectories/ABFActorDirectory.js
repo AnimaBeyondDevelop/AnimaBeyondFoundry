5f9bc038afb4683cbe180f3e3dfaffe81015452c
Julen Bordonaba <julenbordonaba@gmail.com>
2024-08-12 15:42:20 +0200

    REMOVE - Actor directory

--- src/module/SidebarDirectories/ABFActorDirectory.js
+++
@@ -1,7 +0,0 @@
-export default class ABFActorDirectory extends ActorDirectory {
-  constructor(...args) {
-    super(...args);
-    console.log('ABFActorDirectory constructor');
-    // Custom initialization code
-  }
-}
