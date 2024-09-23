9b124666a0d26117e1880e38649a0d6de8bf461e
daraguz <paivaalfonso@gmail.com>
2024-09-23 08:06:41 -0300

    new - iconCheckBox svelte component

---
+++ src/svelte/ui/iconCheckBox.svelte
@@ -0,0 +1,44 @@
+<script>
+  // @ts-nocheck
+  let { icon, value, invert, title, onClick, noStyle, hidden, disabled } = $props();
+  let src = $derived('/systems/animabf/assets/icons/svg/' + icon + '.svg');
+
+  function clickEvent() {
+    value = !value;
+    onClick?.(value);
+  }
+</script>
+
+<input
+  {hidden}
+  class="icon"
+  type="image"
+  {title}
+  {src}
+  alt=""
+  onclick={clickEvent}
+  {disabled}
+  style={(invert ? '--filter:invert(1);' : '') +
+    (noStyle ? '' : !value ? '--opacity:60%;' : '')}
+/>
+
+<style lang="scss">
+  .icon {
+    width: var(--icon-size, 35px);
+    height: var(--icon-size, 35px);
+    margin: var(--icon-margin, 0px);
+    justify-self: center;
+    transition: var(--transition, scale 0.3s ease-out, transform 0.4s ease-out);
+    transform: var(--transform);
+    opacity: var(--opacity);
+    filter: var(--filter);
+    &:hover {
+      scale: 1.05;
+    }
+    &:disabled {
+      &:hover {
+        scale: 1;
+      }
+    }
+  }
+</style>
