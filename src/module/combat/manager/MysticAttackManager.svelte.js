d85137741ff4521c6d02965fc1d843beec9c9e61
daraguz <paivaalfonso@gmail.com>
2024-09-23 08:23:34 -0300

    new - mysticAttackDialog - manager+component

---
+++ src/module/combat/manager/MysticAttackManager.svelte.js
@@ -0,0 +1,168 @@
+import { resistanceEffectCheck } from '@module/combat/utils/resistanceEffectCheck.js';
+import { AttackManager } from '@module/combat/manager/AttackManager.svelte.js'
+import { rollToMessageFlavor } from '@module/combat/utils/rollToMessageFlavor.js'
+import { damageCheck } from '@module/combat/utils/damageCheck.js';
+import ABFFoundryRoll from '@module/rolls/ABFFoundryRoll';
+
+export class MysticAttackManager extends AttackManager {
+    spell = $derived(this.getSpell(this.data.spellUsed));
+    castMethod = $state("accumulated")
+    spellCasting = $derived(
+        this.data.actor.mysticCanCastEvaluate(
+            this.spell,
+            this.data.spellGrade,
+            {
+                prepared: this.castMethod == "prepared",
+                innate: this.castMethod == "innate"
+            },
+            this.castMethod == "override"
+        )
+    )
+    attainableSpellGrades = $derived(this.getAttainableSpellGrades(this.data.spellUsed, this.castMethod));
+
+    constructor(attacker, hooks, options) {
+        super(attacker, hooks, options);
+
+        this.data.attack = {
+            base: this.data.system.mystic.magicProjection.imbalance.offensive.base.value,
+            final: this.data.system.mystic.magicProjection.imbalance.offensive.final.value
+        }
+        this.data.spells = this.data.system.mystic.spells.filter(w => w.system.combatType.value === 'attack')
+
+        const lastOffensiveSpellUsed = this.data.actor.getFlag(
+            'animabf',
+            'lastOffensiveSpellUsed'
+        )
+        if (this.data.spells.find(w => w.id === lastOffensiveSpellUsed)) {
+            this.data.spellUsed = lastOffensiveSpellUsed
+        } else {
+            this.data.spellUsed = this.data.spells[0]?._id
+        }
+
+        const spellCastingOverride = this.data.actor.getFlag(
+            'animabf',
+            'spellCastingOverride'
+        );
+
+        this.castMethod = spellCastingOverride ? "override" : "accumulated"
+
+        this.data.spellGrade = 'base'
+
+        this.onSpellChange()
+    }
+
+    applyModifiers(modifiers) {
+        let totalModifier = super.applyModifiers(modifiers);
+
+        return totalModifier + this.data.attack.final
+    }
+
+    calculateDamage(damageModifiers) {
+        let totalModifier = super.calculateDamage(damageModifiers);
+
+        if (this.data.spellUsed) {
+            let spellUsedEffect =
+                this.spell?.system.grades[this.data.spellGrade].description.value ?? '';
+            return totalModifier + damageCheck(spellUsedEffect)
+        }
+        return totalModifier
+
+    }
+
+    getAttainableSpellGrades(spellUsed, castMethod) {
+        if (castMethod === "override") {
+            return ['base', 'intermediate', 'advanced', 'arcane']
+        } else {
+            let attainableSpellGrades = []
+            let intelligence = this.data.system.characteristics.primaries.intelligence.value
+            let finalIntelligence = this.data.system.mystic.mysticSettings.aptitudeForMagicDevelopment ? intelligence + 3 : intelligence
+            for (let grade in this.spell?.system.grades) {
+                if (finalIntelligence >= this.spell?.system.grades[grade].intRequired.value) {
+                    attainableSpellGrades.push(grade)
+                }
+            }
+            return attainableSpellGrades
+        }
+    }
+
+    getSpell(spellUsed) {
+        if (spellUsed) {
+            return this.data.spells.find(w => w.id === spellUsed)
+        }
+        return undefined
+    }
+
+    onSpellChange() {
+        this.data.critics = {
+            selected: this.spell?.system.critic.value === "-" ? 'none' : this.spell?.system.critic.value ?? 'energy',
+            primary: this.spell?.system.critic.value === "-" ? 'none' : this.spell?.system.critic.value ?? 'energy',
+            secondary: 'none'
+        }
+        this.data.spellGrade = 'base'
+        this.data.visible = this.spell?.system.visible
+        this.getAttainableSpellGrades()
+    }
+
+    onAttack() {
+        this.data.actor.setFlag('animabf', 'spellCastingOverride', this.castMethod === "override");
+        this.data.actor.setFlag('animabf', 'lastOffensiveSpellUsed', this.data.spellUsed)
+
+        const spellUsedEffect = this.spell?.system.grades[this.data.spellGrade].description.value ?? '';
+
+        if (this.data.actor.evaluateCast(this.spellCasting)) {
+            this.castMethod = "override";
+            return;
+        }
+        this.data.resistanceEffect = resistanceEffectCheck(spellUsedEffect);
+
+        let formula = `1d100xa + ${this.attack}`;
+        if (!this.data.withRoll) {
+            // Remove the dice from the formula
+            formula = formula.replace('1d100xa', '0');
+        }
+        if (this.data.attack.base >= 200) {
+            // Mastery reduces the fumble range
+            formula = formula.replace('xa', 'xamastery');
+        }
+
+        const roll = new ABFFoundryRoll(formula, this.data.system);
+
+        roll.roll();
+
+        if (this.data.showRoll) {
+            const { i18n } = game;
+
+            const flavor = i18n.format('macros.combat.dialog.magicAttack.title', {
+                spell: rollToMessageFlavor(this.spell?.name, roll)
+            })
+
+            roll.toMessage({
+                speaker: ChatMessage.getSpeaker({ token: this.data.token }),
+                flavor
+            });
+        }
+
+        const rolled = roll.total - this.attack;
+
+        this.hooks.onAttack({
+            type: 'mystic',
+            values: {
+                spellUsed: this.data.spellUsed,
+                spellGrade: this.data.spellGrade,
+                spellName: this.spell?.name,
+                magicProjection: this.attack,
+                spellCasting: this.spellCasting,
+                damage: this.damage,
+                critic: this.data.critics.selected,
+                roll: rolled,
+                total: roll.total,
+                fumble: roll.fumbled,
+                resistanceEffect: this.data.resistanceEffect,
+                visible: this.data.visible,
+                projectile: this.data.projectile,
+                distance: this.data.distance,
+                attackerCombatMod: this.modifiers //rev hace falta agregar damageModifiers? prob si
+            }
+        });
+    }
+}
---
+++ src/svelte/components/mysticAttackDialog.svelte
@@ -0,0 +1,202 @@
+<script>
+  // @ts-nocheck
+  import CardSelect from '@svelte/ui/card/cardSelect.svelte';
+  import IconInput from '@svelte/ui/iconInput.svelte';
+  import CardMarkerCritic from '@svelte/ui/card/cardMarkerCritic.svelte';
+  import IconCheckBox from '@svelte/ui/iconCheckBox.svelte';
+  import CardButton from '@svelte/ui/card/cardButton.svelte';
+  import CardCircle from '@svelte/ui/card/cardCircle.svelte';
+  import CardCombat from '@svelte/ui/card/cardCombat.svelte';
+  import IconSwitch from '@svelte/ui/iconSwitch.svelte';
+
+  let { manager } = $props();
+  const i18n = game.i18n;
+
+  let markerWidth = { min: '150px', max: '435px' };
+  let togglePanel = $state(false);
+</script>
+
+<div class="template">
+  <g class="background">
+    <CardCombat
+      width={togglePanel ? '650px' : '500px'}
+      sidebar={togglePanel ? '210px' : '60px'}
+    ></CardCombat>
+  </g>
+  <div class="sidebar">
+    <div></div>
+    <div></div>
+  </div>
+  <div class="box"></div>
+  <g class="select">
+    <CardSelect
+      bind:selection={manager.data.spellUsed}
+      options={manager.data.spells}
+      onChange={value => manager.onSpellChange(value)}
+      >{#if manager.data.spells.length === 0}
+        <option>No Spell Found</option>
+      {/if}</CardSelect
+    >
+  </g>
+  <g class="marker">
+    <CardMarkerCritic
+      value={manager.damage}
+      bind:modifier={manager.damageModifiers.special.modifier}
+      bind:critics={manager.data.critics}
+      {markerWidth}
+    />
+  </g>
+  <g class="primary">
+    <IconInput
+      icon="mystic"
+      value={manager.attack}
+      bind:modifier={manager.modifiers.special.modifier}
+      title={i18n.localize('anima.ui.mystic.magicProjection.final.title')}
+    />
+  </g>
+  <div class="secondary">
+    <IconInput
+      icon="zeon-cost"
+      value={manager.spellCasting.zeon.cost}
+      title={i18n.localize('anima.ui.mystic.spell.grade.zeon.title')}
+      disabled={true}
+      --icon-size="45px"
+    />
+  </div>
+  <g class="upper-left">
+    <IconInput
+      icon="zeon-accumulated"
+      value={manager.spellCasting.zeon.accumulated}
+      title={i18n.localize('macros.combat.dialog.zeonAccumulated.title')}
+      disabled={true}
+    />
+  </g>
+  <div class="spell-grade">
+    {#each manager.attainableSpellGrades as spellGrade, i}
+      <input
+        class={manager.data.spellGrade === spellGrade ? 'selected' : ''}
+        title={i18n.localize(`macros.combat.dialog.spellGrade.${spellGrade}.title`)}
+        type="image"
+        src={`/systems/animabf/assets/icons/svg/spell-${spellGrade}.svg`}
+        alt={spellGrade}
+        onclick={() => (manager.data.spellGrade = spellGrade)}
+        style={i < 2 ? '--icon-size:26px' : ''}
+      />
+    {/each}
+  </div>
+  <div class="button">
+    <CardButton title={'Atacar'} onClick={() => manager.onAttack()} />
+  </div>
+  <div class="circle">
+    <CardCircle>
+      <IconSwitch
+        icons={['cast-accumulated', 'cast-prepared', 'cast-innate', 'cast-override']}
+        bind:value={manager.castMethod}
+        options={['accumulated', 'prepared', 'innate', 'override']}
+        title={i18n.localize(`dialogs.castSpell.${manager.castMethod}.title`)}
+        --icon-size="30px"
+      />
+    </CardCircle>
+  </div>
+</div>
+
+<style lang="scss">
+  .template {
+    height: 300px;
+    width: 500px;
+    display: grid;
+    grid-template: 2fr 2fr 2fr 2.8fr/65px 150px 150px 1fr;
+    gap: 5px;
+
+    .background {
+      grid-area: 1 / 1 / -1 / -1;
+      justify-self: end;
+    }
+    .box {
+      grid-area: 1 / 3 / 1 / -1;
+      justify-self: end;
+      align-self: center;
+      margin-right: 40px;
+      margin-top: 5px;
+      z-index: 1;
+    }
+    .select {
+      grid-area: 3 / 1 / 4 /-1;
+      justify-self: end;
+      align-self: center;
+    }
+    .marker {
+      grid-area: 3 / 1 / 4 /-1;
+      justify-self: end;
+      align-self: center;
+      margin: -5px -5px 0;
+      z-index: 1;
+    }
+    .marker {
+      width: --marker-widht;
+    }
+    .sidebar {
+      width: 60px;
+      grid-area: 1 / 1 / span 4;
+      place-self: start end;
+      padding: 15px;
+      display: grid;
+      grid-template: 30px 30px 40px 55px 40px / 1fr;
+      gap: 5px;
+      place-items: center;
+    }
+
+    .primary {
+      display: grid;
+      grid-area: 2/2;
+      place-self: end start;
+      z-index: 1;
+    }
+
+    .secondary {
+      display: grid;
+      grid-area: 2/3;
+      place-self: end start;
+      z-index: 1;
+    }
+
+    .upper-left {
+      display: grid;
+      grid-area: 1/2;
+      place-self: end start;
+      z-index: 1;
+    }
+    .spell-grade {
+      display: flex;
+      grid-area: 4/2;
+      height: 30px;
+      place-items: center;
+      z-index: 1;
+      input {
+        width: var(--icon-size, 30px);
+        height: var(--icon-size, 30px);
+        margin-left: 10px;
+        transition: scale 0.3s ease-out;
+        opacity: 60%;
+        &.selected {
+          opacity: 1;
+        }
+        &:hover {
+          scale: 1.05;
+        }
+      }
+    }
+    .button {
+      grid-area: 4 / 3 / 5 / 5;
+      justify-self: end;
+      align-self: center;
+      margin-right: -25px;
+    }
+    .circle {
+      grid-area: 4 / 3;
+      justify-self: center;
+      align-self: center;
+      margin-right: -25px;
+    }
+  }
+</style>
