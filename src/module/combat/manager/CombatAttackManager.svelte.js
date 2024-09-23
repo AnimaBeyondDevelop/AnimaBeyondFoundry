7e9f43943c6a2b88c481b45ee184c289f0126d70
daraguz <paivaalfonso@gmail.com>
2024-09-23 08:23:14 -0300

    new - combatAttackDialog - manager+component

---
+++ src/module/combat/manager/CombatAttackManager.svelte.js
@@ -0,0 +1,151 @@
+import { resistanceEffectCheck } from '@module/combat/utils/resistanceEffectCheck.js';
+import { AttackManager } from '@module/combat/manager/AttackManager.svelte.js'
+import { rollToMessageFlavor } from '@module/combat/utils/rollToMessageFlavor.js'
+import ABFFoundryRoll from '@module/rolls/ABFFoundryRoll';
+
+export class CombatAttackManager extends AttackManager {
+    weapon = $derived(this.getWeapon(this.data.weaponUsed));
+    constructor(attacker, hooks, options) {
+        super(attacker, hooks, options);
+
+        this.data.attack = {
+            base: this.data.system.combat.attack.base.value,
+            final: this.data.system.combat.attack.final.value
+        }
+        this.data.fatigue = { used: 0, available: this.data.system.characteristics.secondaries.fatigue.value };
+        this.data.weapons = this.data.system.combat.weapons
+
+        const lastOffensiveWeaponUsed = this.data.actor.getFlag(
+            'animabf',
+            'lastOffensiveWeaponUsed'
+        )
+        if (this.data.weapons.find(w => w.id === lastOffensiveWeaponUsed)) {
+            this.data.weaponUsed = lastOffensiveWeaponUsed
+        } else {
+            this.data.weaponUsed = this.data.weapons[0]?._id ?? "unarmed"
+        }
+
+        this.data.highGround = false
+        this.data.poorVisibility = false
+        this.data.targetInCover = false
+
+        this.onWeaponChange()
+    }
+
+    applyModifiers(modifiers) {
+        let totalModifier = super.applyModifiers(modifiers);
+
+        if (this.data.weaponUsed === "unarmed") {
+            return this.data.attack.final + totalModifier;
+        } else {
+            return this.weapon.system.attack.final.value + totalModifier
+        }
+    }
+
+    calculateDamage(damageModifiers) {
+        let totalModifier = super.calculateDamage(damageModifiers);
+
+        if (this.data.weaponUsed === "unarmed") {
+            return 10 + this.data.system.characteristics.primaries.strength.mod + totalModifier
+        } else {
+            return this.weapon.system.damage.final.value + totalModifier
+        }
+    }
+
+    getWeapon(weaponUsed) {
+        if (weaponUsed === "unarmed") {
+            return undefined
+        } else {
+            return this.data.weapons.find(w => w.id === weaponUsed)
+        }
+    }
+
+    onWeaponChange() {
+        this.data.critics = {
+            selected: this.weapon?.system.critic.primary.value === '-' ? 'none' : this.weapon?.system.critic.primary.value ?? 'impact',
+            primary: this.weapon?.system.critic.primary.value === '-' ? 'none' : this.weapon?.system.critic.primary.value ?? 'impact',
+            secondary: this.weapon?.system.critic.secondary.value === '-' ? 'none' : this.weapon?.system.critic.secondary.value ?? 'impact',
+        }
+        if (this.weapon?.system.isRanged.value) {
+            let shotType = this.weapon.system.shotType.value
+            this.data.projectile.value = shotType === "shot" ? true : false
+            this.data.projectile.type = shotType
+        } else {
+            this.data.projectile.value = false
+            this.data.projectile.type = ""
+            this.resetProjectileModifiers()
+        }
+    }
+
+    onCriticChange(critic) {
+        let secondaryCritic = this.data.critics.secondary === critic ? 1 : 0;
+        this.addModfier("secondaryCritic", secondaryCritic)
+    }
+
+    resetProjectileModifiers() {
+        this.addModfier("poorVisibility", 0)
+        this.addModfier("targetInCover", 0)
+        this.data.poorVisibility = false
+        this.data.targetInCover = false
+    }
+
+    onAttack() {
+        this.data.actor.setFlag('animabf', 'lastOffensiveWeaponUsed', this.data.weaponUsed)
+        if (this.weapon !== undefined) {
+            this.data.resistanceEffect = resistanceEffectCheck(this.weapon.system.special.value);
+        }
+
+        let formula = `1d100xa + ${this.attack}`;
+        if (!this.data.withRoll) {
+            // Remove the dice from the formula
+            formula = formula.replace('1d100xa', '0');
+        }
+        if (this.data.attack.base >= 200) {
+            // Mastery reduces the fumble range
+            formula = formula.replace('xa', 'xamastery');
+        }
+
+        const roll = new ABFFoundryRoll(formula, this.data.system);
+
+        roll.roll();
+
+        if (this.data.showRoll) {
+            const { i18n } = game;
+
+            const flavor = this.weapon
+                ? i18n.format('macros.combat.dialog.physicalAttack.title', {
+                    weapon: rollToMessageFlavor(this.weapon?.name, roll)
+                })
+                : i18n.format('macros.combat.dialog.physicalAttack.unarmed.title', {
+                    unarmed: rollToMessageFlavor(i18n.format('macros.combat.dialog.unarmed.title'), roll)
+                });
+
+            roll.toMessage({
+                speaker: ChatMessage.getSpeaker({ token: this.data.token }),
+                flavor
+            });
+        }
+
+        const rolled = roll.total - this.attack;
+
+        this.hooks.onAttack({
+            type: 'combat',
+            values: {
+                unarmed: this.data.weaponUsed === 'unarmed', //rev
+                damage: this.damage,
+                attack: this.attack,
+                weaponUsed: this.data.weaponUsed,
+                critic: this.data.critics.selected,
+                fatigueUsed: this.data.fatigue.used,
+                roll: rolled,
+                total: roll.total,
+                fumble: roll.fumbled,
+                resistanceEffect: this.data.resistanceEffect,
+                visible: this.data.visible,
+                projectile: this.data.projectile,
+                distance: this.data.distance,
+                attackerCombatMod: this.modifiers //rev hace falta agregar damageModifiers? prob si
+            }
+        });
+    }
+}
---
+++ src/svelte/components/combatAttackDialog.svelte
@@ -0,0 +1,207 @@
+<script>
+  // @ts-nocheck
+  import CardSelect from '@svelte/ui/card/cardSelect.svelte';
+  import IconInput from '@svelte/ui/iconInput.svelte';
+  import IconBox from '@svelte/ui/iconBox.svelte';
+  import CardMarkerCritic from '@svelte/ui/card/cardMarkerCritic.svelte';
+  import IconCheckBox from '@svelte/ui/iconCheckBox.svelte';
+  import CardButton from '@svelte/ui/card/cardButton.svelte';
+  import CardCircle from '@svelte/ui/card/cardCircle.svelte';
+  import CardCombat from '@svelte/ui/card/cardCombat.svelte';
+
+  let { manager } = $props();
+  const i18n = game.i18n;
+
+  let markerWidth = { min: '150px', max: '435px' };
+  let togglePanel = $state(false);
+</script>
+
+<div class="template">
+  <g class="background">
+    <CardCombat
+      width={togglePanel ? '650px' : '500px'}
+      sidebar={togglePanel ? '210px' : '60px'}
+    ></CardCombat>
+  </g>
+  <div class="sidebar">
+    <div></div>
+    <div></div>
+    <!-- 
+
+    Código Necesario para el update de ataques específicos.
+    Agrande el tamaño de la CardCombat dando la ilusión de que la barra lateral se expande.
+
+    <IconCheckBox
+      icon="arrow"
+      bind:value={togglePanel}
+      title={togglePanel ?  i18n.localize('anima.ui.togglePanel.close.title') :  i18n.localize('anima.ui.togglePanel.open.title')}
+      invert={true}
+      noStyle={true}
+      --icon-size="30px"
+      --icon-margin="-5px"
+      --transform={!togglePanel ? 'scaleX(-1)' : ''}
+    /> -->
+  </div>
+  <div class="box">
+    <IconBox
+      icon="fatigue"
+      bind:activeIcons={manager.data.fatigue.used}
+      quantity={Math.min(manager.data.fatigue.available, 5)}
+      onChange={value => manager.addModfier("fatigue", value)}
+      title={i18n.localize('macros.combat.dialog.fatigue.title') +
+        ` (${manager.data.fatigue.available})`}
+    />
+  </div>
+  <g class="select">
+    <CardSelect
+      bind:selection={manager.data.weaponUsed}
+      options={manager.data.weapons}
+      onChange={value => manager.onWeaponChange(value)}
+      ><option value="unarmed">{i18n.format('macros.combat.dialog.unarmed.title')}</option></CardSelect
+    >
+  </g>
+  <g class="marker">
+    <CardMarkerCritic
+      value={manager.damage}
+      bind:modifier={manager.damageModifiers.special.modifier}
+      bind:critics={manager.data.critics}
+      {markerWidth}
+      onChange={value => manager.onCriticChange(value)}
+    />
+  </g>
+  <g class="primary">
+    <IconInput
+      icon="attack"
+      value={manager.attack}
+      bind:modifier={manager.modifiers.special.modifier}
+      title={i18n.localize('macros.combat.dialog.attack.title')}
+    />
+  </g>
+  <div class="right-icons">
+    <IconCheckBox
+      icon="high-ground"
+      bind:value={manager.data.highGround}
+      title={i18n.localize('macros.combat.dialog.highGround.title')}
+      onClick={value => {
+        manager.addModfier("highGround", value);
+      }}
+      --icon-size="30px"
+    />
+    <IconCheckBox
+      icon="target-in-cover"
+      bind:value={manager.data.targetInCover}
+      title={i18n.localize('macros.combat.dialog.targetInCover.title')}
+      hidden={!manager.weapon?.system.isRanged.value || !manager.data.projectile.value}
+      onClick={value => {
+        manager.addModfier("targetInCover", value);
+      }}
+    />
+    <IconCheckBox
+      icon="poor-visibility"
+      bind:value={manager.data.poorVisibility}
+      title={i18n.localize('macros.combat.dialog.poorVisibility.title')}
+      hidden={!manager.weapon?.system.isRanged.value || !manager.data.projectile.value}
+      onClick={value => {
+        manager.addModfier("poorVisibility", value);
+      }}
+    />
+  </div>
+  <div class="button">
+    <CardButton title={'Atacar'} onClick={() => manager.onAttack()} />
+  </div>
+  {#if manager.data.projectile.type === "throw"}
+    <div class="circle">
+      <CardCircle>
+        <IconCheckBox
+          icon={manager.data.projectile.value? "throw" : "no-throw"}
+          bind:value={manager.data.projectile.value}
+          title={manager.data.projectile.value? i18n.localize("macros.combat.dialog.throw.title") : i18n.localize("macros.combat.dialog.melee.title")}
+          noStyle={true}
+          --icon-size={manager.data.projectile.value? "32px" : "22px"}
+          --transform={manager.data.projectile.value? "rotate(360deg)" : "rotate(0deg)"}
+        />
+      </CardCircle>
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  .template {
+    height: 300px;
+    width: 500px;
+    display: grid;
+    grid-template: 2fr 2fr 2fr 2.8fr/65px 150px 150px 1fr;
+    gap: 5px;
+
+    .background {
+      grid-area: 1 / 1 / -1 / -1;
+      justify-self: end;
+    }
+    .box {
+      grid-area: 1 / 3 / 1 / -1;
+      justify-self: end;
+      align-self: center;
+      margin-right: 40px;
+      margin-top: 5px;
+      z-index: 1;
+    }
+    .select {
+      grid-area: 3 / 1 / 4 /-1;
+      justify-self: end;
+      align-self: center;
+    }
+    .marker {
+      grid-area: 3 / 1 / 4 /-1;
+      justify-self: end;
+      align-self: center;
+      margin: -5px -5px 0;
+      z-index: 1;
+    }
+    .marker {
+      width: --marker-widht;
+    }
+    .sidebar {
+      width: 60px;
+      grid-area: 1 / 1 / span 4;
+      place-self: start end;
+      padding: 15px;
+      display: grid;
+      grid-template: 30px 30px 40px 55px 40px / 1fr;
+      gap: 5px;
+      place-items: center;
+    }
+
+    .primary {
+      display: grid;
+      grid-area: 2/2;
+      place-self: end start;
+      z-index: 1;
+    }
+
+    .right-icons {
+      display: flex;
+      flex-direction: row-reverse;
+      width: 255px;
+      grid-area: 2 / 3;
+      margin-right: 15px;
+      margin-bottom: -5px;
+      gap: 10px;
+      align-items: center;
+      justify-content: end;
+      z-index: 1;
+    }
+
+    .button {
+      grid-area: 4 / 3 / 5 / 5;
+      justify-self: end;
+      align-self: center;
+      margin-right: -25px;
+    }
+    .circle {
+      grid-area: 4 / 3;
+      justify-self: center;
+      align-self: center;
+      margin-right: -25px;
+    }
+  }
+</style>
