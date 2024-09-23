f1cc9ae673fe84b3b83e961e9f99436bde79aecb
daraguz <paivaalfonso@gmail.com>
2024-09-23 08:25:45 -0300

    new - SvelteAttackDialog

---
+++ src/module/dialogs/combat/SvelteAttackDialog.js
@@ -0,0 +1,46 @@
+import { Templates } from "@module/utils/constants";
+import { mount } from "svelte";
+import AttackDialog from "@svelte/components/attackDialog.svelte";
+
+export class SvelteAttackDialog extends Application {
+  constructor(attacker, hook, bonus) {
+    super()
+    this.data = { attacker, hook, bonus }
+    this.render(true)
+  }
+
+  async _render(force = false, options = {}) {
+    return super._render(force, options).then(v => {
+      let element = document.getElementById("svelte-app");
+      if (element) {
+        let props = { data: this.data }
+        mount(AttackDialog, { target: element, props })
+      }
+      return v
+    })
+  }
+
+
+  /** @inheritdoc */
+  getData() {
+    return this.data
+  }
+
+  static get defaultOptions() {
+    return mergeObject(super.defaultOptions, {
+      classes: ['overflow'],
+      template: Templates.Svelte.SvelteApp,
+      width: 430,
+      height: 230,
+    });
+  }
+
+  async close(options) {
+    if (options?.force) {
+      return super.close(options);
+    }
+
+    // eslint-disable-next-line no-useless-return,consistent-return
+    return;
+  }
+}
