a9fc60140f345c90d4e4c2a50c14a5960196c763
daraguz <paivaalfonso@gmail.com>
2024-09-23 08:09:26 -0300

    new - iconBox svelte component

---
+++ src/svelte/ui/iconBox.svelte
@@ -0,0 +1,59 @@
+<script>
+  // @ts-nocheck
+  let { icon, activeIcons, quantity, title, onChange } = $props();
+  let iconOff = '/systems/animabf/assets/icons/svg/' + icon + '.svg';
+  let iconOn = '/systems/animabf/assets/icons/svg/' + icon + '_fill.svg';
+
+  function iconSwitch(i) {
+    if (activeIcons === i + 1) {
+      activeIcons = 0;
+    } else activeIcons = i + 1;
+    onChange?.(activeIcons);
+  }
+</script>
+
+<div class="content" style="--quantity:{quantity};">
+  {#each { length: quantity } as _, i}
+    {#if i < activeIcons}
+      <input
+        class="icon"
+        type="image"
+        {title}
+        onclick={() => iconSwitch(i)}
+        src={iconOn}
+        alt=""
+      />
+    {:else}
+      <input
+        class="icon off"
+        type="image"
+        {title}
+        onclick={() => iconSwitch(i)}
+        src={iconOff}
+        alt=""
+      />
+    {/if}
+  {/each}
+</div>
+
+<style lang="scss">
+  .content {
+    display: grid;
+    grid-template: 1fr / repeat(var(--quantity), 1fr);
+    place-items: center;
+    gap: var(--gap);
+
+    .icon {
+      height: var(--icon-size, 30px);
+      justify-self: right;
+      transition: var(--transition, scale 0.3s ease-out, transform 0.4s ease-out);
+      transform: var(--transform);
+      &:hover {
+        scale: 1.05;
+      }
+      &.off {
+        opacity: 60%;
+      }
+    }
+  }
+</style>
