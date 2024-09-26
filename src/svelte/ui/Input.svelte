683e326bb55d692b6c1a0fbd0df9c2013a04e855
daraguz <paivaalfonso@gmail.com>
2024-09-26 13:03:40 -0300

    new - input and cardMarker svelte components

---
+++ src/svelte/ui/Input.svelte
@@ -0,0 +1,47 @@
+<script>
+  // @ts-nocheck
+  let { value, modifier = $bindable(0), title, disabled } = $props();
+
+  function onchange(e) {
+    e.target.blur();
+    const input = e.target.value;
+    if (['+', '-'].includes(input.slice(0, 1))) {
+      modifier += parseInt(input);
+    } else if (input === '') {
+      modifier = 0;
+    } else {
+      modifier = parseInt(input) - value;
+    }
+  }
+</script>
+
+<input
+  class="input"
+  {title}
+  {value}
+  type="text"
+  onfocus={e => e.target.select()}
+  {onchange}
+  {disabled}
+/>
+
+<style lang="scss">
+  .input {
+    all: unset;
+    width: var(--input-width, 85px);
+    color: white;
+    font-size: var(--font-size, 40px);
+    -webkit-text-stroke: 4px black;
+    paint-order: stroke fill;
+    text-align: center;
+    &:focus {
+      all: unset;
+      width: var(--input-width, 85px);
+      color: white;
+      font-size: var(--font-size, 40px);
+      -webkit-text-stroke: 4px black;
+      paint-order: stroke fill;
+      text-align: center;
+    }
+  }
+</style>
---
+++ src/svelte/ui/card/cardMarker.svelte
@@ -0,0 +1,49 @@
+<script>
+  // @ts-nocheck
+  let {
+    height,
+    width,
+    border,
+    children
+  } = $props();
+
+</script>
+
+<div class="marker"
+style={`--height:${height || '80px'};
+--width:${width || '150px'};
+--border:${border || '5px'};`}>
+  <div class="body">
+    {#if children}
+      {@render children()}
+    {/if}
+  </div>
+</div>
+
+<style lang="scss">
+  .marker {
+    height: var(--height);
+    width: var(--width);
+    position: relative;
+    background: black;
+    clip-path: polygon(37px 0, 100% 0, 100% 100%, 37px 100%, 0 40px);
+    transition: width 1s ease-in-out;
+
+    .body {
+      display: grid;
+      grid-template-rows: 1fr;
+      gap: 8px;
+      justify-content: end;
+      place-items: center;
+      height: calc(var(--height) - var(--border) * 2);
+      width: calc(var(--width) - var(--border) * 2);
+      position: absolute;
+      top: var(--border);
+      left: var(--border);
+      background: rgb(45, 45, 45);
+      clip-path: polygon(35px 0, 100% 0, 100% 100%, 35px 100%, 3px 35px);
+      transition: width 1s ease-in-out;
+      z-index: -1;
+    }
+  }
+</style>
