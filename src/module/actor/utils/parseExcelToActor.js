4b0fc54fc30aeff1109ca40e447a237263c55897
Julen_BZ <julen@bravezebra.es>
2024-11-07 19:15:14 +0100

    fix: martial art tables breaking importer

--- src/module/actor/utils/parseExcelToActor.js
+++ src/module/actor/utils/parseExcelToActor.js
@@ -1008,19 +1008,29 @@ export const parseExcelToActor = async (excelData, actor) => {
     });
   }
 
+  
+
   for (var i = 0; i < artesMarciales.length; i++) {
-    const arteMarcialSeparada = artesMarciales[i]
-      .split('(')
-      .map(value => value.trim())
-      .filter(element => element !== '');
-    const grade = arteMarcialSeparada[1].replace(/[ \)]+/g, '');
-    await actor.createInnerItem({
-      name: arteMarcialSeparada[0],
-      type: ABFItems.MARTIAL_ART,
-      system: {
-        grade: { value: grade }
-      }
-    });
+    if (artesMarciales[i].includes("Tabla de Armas:")) {
+      await actor.createInnerItem({
+        name: artesMarciales[i],
+        type: ABFItems.COMBAT_TABLE
+      });
+    }
+    else {
+      const arteMarcialSeparada = artesMarciales[i]
+        .split('(')
+        .map(value => value.trim())
+        .filter(element => element !== '');
+      const grade = arteMarcialSeparada[1].replace(/[ \)]+/g, '');
+      await actor.createInnerItem({
+        name: arteMarcialSeparada[0],
+        type: ABFItems.MARTIAL_ART,
+        system: {
+          grade: { value: grade }
+        }
+      });
+    }
   }
 
   // for (var i = 0; i < tecnicasKi.length; i++) {
