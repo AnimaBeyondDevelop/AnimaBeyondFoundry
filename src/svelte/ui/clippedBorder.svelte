cdf570f933bc740207a53867ef4de70d6bb0f675
A. Balmaseda <aitor.5502.b@gmail.com>
2024-11-13 01:06:26 +0100

    Refactor: clipping mixing to scss file `clipping.cscc`

--- src/svelte/ui/clippedBorder.svelte
+++ src/svelte/ui/clippedBorder.svelte
@@ -14,46 +14,21 @@
 </div>
 
 <style lang="scss">
-  @use 'sass:list';
+  @use 'clipping';
   $height: var(--height, 100px);
   $width: var(--width, 100px);
   $border: var(--border, 5px);
   $edge: var(--edge, 40px);
   $bg-color: var(--bg-color, $background-main);
   $border-color: var(--border-color, $border-color);
-
-  @function corner($coord-x, $coord-y, $slant-param) {
-    @if $coord-x != $coord-y {
-      @return (
-        calc($coord-x - $slant-param) $coord-y,
-        $coord-x calc($coord-y + $slant-param)
-      );
-    }
-    @return (
-      $coord-x calc($coord-y + $slant-param),
-      calc($coord-x + $slant-param) $coord-y
-    );
-  }
-
-  @mixin slanted-clip($edge, $border, $corners: 0 1 1 1, $outer: 0) {
-    $offset: calc(($border * sqrt(2) - 2 * $border) * $outer);
-    $slant-param: calc($edge - $offset);
-    $clip: polygon(
-      corner(0%, 0%, calc(list.nth($corners, 1) * $slant-param)),
-      corner(100%, 0%, calc(list.nth($corners, 2) * $slant-param)),
-      corner(100%, 100%, calc(-1 * list.nth($corners, 3) * $slant-param)),
-      corner(0%, 100%, calc(-1 * list.nth($corners, 4) * $slant-param))
-    );
-    -webkit-clip-path: $clip;
-    clip-path: $clip;
-  }
+  $corners: 1 1 0 1;
 
   .clipped-border {
     position: relative;
     width: $width;
     height: $height;
     background: $border-color;
-    @include slanted-clip($edge, $border, $outer: 1);
+    @include clipping.slanted-edges($edge, $border, $corners, $outer: 1);
     transition: width 1s ease-in-out;
 
     &::before {
@@ -61,7 +36,7 @@
       position: absolute;
       inset: $border;
       background: $bg-color;
-      @include slanted-clip($edge, $border);
+      @include clipping.slanted-edges($edge, $border, $corners);
     }
   }
 </style>
