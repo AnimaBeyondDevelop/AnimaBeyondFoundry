72ed56dfbd5cfd569fbe2cb56bbd4410a1bc735c
A. Balmaseda <aitor.5502.b@gmail.com>
2025-01-03 12:25:05 +0100

    CombatCard componnent rework. Damage marker still missing

---
+++ src/svelte/ui/card/combatCard.svelte
@@ -0,0 +1,166 @@
+<script>
+  /**
+   * @import {Snippet} from 'svelte'
+   * @import { FormEventHandler } from 'svelte/elements';
+   *
+   * @typedef {Object} AttackOption
+   * @property {string} name
+   *
+   * @typedef {Object} ButtonSpec
+   * @property {string} [text]
+   * @property {string} [icon]
+   * @property {FormEventHandler<HTMLButtonElement>} onclick
+   *
+   * @typedef {Object} CombatCardButton
+   * @property {string} location
+   * @property {ButtonSpec} props
+   * @property {ButtonSpec} [secondary]
+   *
+   * @typedef {Object} Props
+   * @property {Snippet} [sidebar]
+   * @property {Snippet} [top]
+   * @property {Snippet} [selector]
+   * @property {Snippet} [bottom]
+   * @property {CombatCardButton[]} buttons
+   */
+
+  import Card from '@svelte/ui/card/card.svelte';
+  import CardButton from '@svelte/ui/card/cardButton.svelte';
+
+  /** @type {Props} */
+  let { sidebar, top, selector, bottom, buttons } = $props();
+</script>
+
+<!--
+@component
+Combat card component. It defines a card component with a body divided into a top,
+selector (middle) and bottom regions. It also adds combat buttons around the card.
+
+Notes:
+- It reads styles from `./card.scss`, but their value can overwritten by passings custom css properties
+to this component:
+```tsx
+  <script>
+    const buttonSpecs = [
+      { location: 'sidebar', props: { icon: 'dice', onclick: () => {} } },
+      { location: 'separator', props: { icon: 'distance', onclick: () => {} } },
+      {
+        location: 'main',
+        props: { text: 'Attack', onclick: () => {} },
+        secondary: { icon: 'no-throw', onclick: () => {} }
+      }
+    ];
+    let attackOptions = [{ name: 'Espada larga' }, { name: 'Descarga de luz' }];
+    let selectedAttack = $state(attackOptions[0]);
+  </script>
+  <CombatCard {buttons} --width=300px --border-size=10px >
+    {#snippet selector()}
+      <CardSelect options={attackOptions} bind:value={selectedAttack} />
+    {/snippet}
+
+    {#snippet top()}
+      <h1> Attacking! </h1>
+    {/snippet}
+  </CombatCard>
+```
+-->
+<div class="card-container">
+  <Card {sidebar}>
+    {#snippet body()}
+      <div class="body">
+        <div class="top">{@render top?.()}</div>
+        <div class="selector">{@render selector?.()}</div>
+        <div class="bottom">{@render bottom?.()}</div>
+      </div>
+    {/snippet}
+  </Card>
+
+  {#each buttons as { location, secondary, props }}
+    <div class={`button-${location}`}>
+      <CardButton {...props} shape={location === 'main' ? 'angled' : 'circle'} />
+      {#if secondary}
+        <CardButton {...secondary} class="secondary" shape="circle" />
+      {/if}
+    </div>
+  {/each}
+</div>
+
+<style lang="scss">
+  @use 'card';
+  @use 'borders';
+
+  $height: card.$card-height;
+  $width: card.$card-width;
+  $edge: card.$edge-size;
+  $sidebar: card.$sidebar-size;
+  $border: card.$border-size;
+  $border-color: card.$border-color;
+  $bg-color: card.$background-color;
+  $sidebar-color: card.$sidebar-color;
+
+  @mixin button-container($height) {
+    justify-self: end;
+    align-self: center;
+
+    --height: #{$height};
+    height: $height;
+    width: fit-content;
+  }
+
+  .card-container :global {
+    width: fit-content;
+    height: fit-content;
+    position: relative;
+
+    @include card.text();
+
+    .content {
+      .body {
+        height: 100%;
+        width: calc($width - $border - $edge);
+        display: flex;
+        flex-direction: column;
+
+        .top {
+          height: 55%;
+        }
+
+        .selector {
+          flex-shrink: 0;
+        }
+        .bottom {
+          height: 30%;
+        }
+      }
+    }
+
+    .button-sidebar {
+      @include button-container(32px);
+      position: absolute;
+      left: calc(($border * 0.2) - $height / 2);
+      bottom: calc($edge + ($border * 1.5) - $height / 2);
+    }
+
+    .button-separator {
+      @include button-container(32px);
+      position: absolute;
+      bottom: calc(($border * 1.5) - $height / 2);
+      left: calc($sidebar + ($border * 1.5) - $height / 2);
+    }
+
+    .button-main {
+      @include button-container(50px);
+      display: flex;
+      flex-shrink: 0;
+      align-items: center;
+      position: absolute;
+      bottom: calc(($border - $height) / 2);
+      right: calc(($border - $height) / 2);
+      .secondary {
+        position: absolute;
+        left: -16px;
+        --height: 38px;
+      }
+    }
+  }
+</style>
