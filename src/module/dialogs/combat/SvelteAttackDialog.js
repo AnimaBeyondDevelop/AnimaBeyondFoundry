f1947d82381a35bbaa7c1940a952b08fc2d100d1
A. Balmaseda <aitor.5502.b@gmail.com>
2025-02-20 18:35:34 +0100

    cleanup: remove old svelte attack components

--- src/module/dialogs/combat/SvelteAttackDialog.js
+++
@@ -1,9 +0,0 @@
-// TODO: remove this file once sockets are updated, since it is not used anymore
-export class SvelteAttackDialog extends Application {
-  constructor(attacker, defender, hook, bonus) {
-    super();
-    this.data = { attacker, defender, hook, bonus };
-    this.render(true);
-  }
-}
-
