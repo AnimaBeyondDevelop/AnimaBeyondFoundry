65482ff158b36236d5d50de5df38f64dfbed608f
daraguz <paivaalfonso@gmail.com>
2025-04-30 11:48:51 -0300

    refactor: use enums in ABFConfig

--- src/module/ABFConfig.js
+++ src/module/ABFConfig.js
@@ -1,16 +1,16 @@
 import {
+  WeaponEquippedHandType,
+  WeaponKnowledgeType,
   WeaponManageabilityType,
   WeaponShotType,
   WeaponSizeProportion,
   WeaponSize
-} from './types/combat/WeaponItemConfig';
-import { NoneCriticType, CriticType } from './types/combat/CriticType';
-import {
-  PsychicPowerActionTypes,
-  PsychicPowerDisciplines,
-  PsychicPowerCombatTypes
-} from './types/psychic/PsychicPowerItemConfig.js';
-import { SpellGrades } from './types/mystic/SpellItemConfig';
+} from '@module/data/items/enums/WeaponEnums';
+import { NoneCriticType, CriticType } from '@module/data/items/enums/CriticEnums';
+import { PsychicDiscipline } from '@module/data/items/enums/PsychicPowerEnums';
+import { SpellGrade, SpellType, Via } from '@module/data/items/enums/SpellEnums';
+import { ResistanceType } from '@module/data/items/enums/ResistanceTypeEnum';
+import { CombatType, ActionType } from '@module/data/items/enums/CombatEnums';
 
 const criticTypes = /** @type {const} */ ({
   [CriticType.CUT]: 'anima.ui.combat.armors.at.cut.title',
@@ -30,7 +30,37 @@ export const ABFConfig = /** @type {const} */ ({
         [NoneCriticType.NONE]: 'anima.ui.combat.armors.at.none.title',
         ...criticTypes
       },
+      combatTypes: {
+        //TODO: Create new localization for this
+        [CombatType.ATTACK]: 'anima.ui.psychic.psychicPowers.combatType.attack.title',
+        [CombatType.DEFENSE]: 'anima.ui.psychic.psychicPowers.combatType.defense.title',
+        [CombatType.NONE]: 'anima.ui.psychic.psychicPowers.combatType.none.title'
+      },
+      //TODO: Create new localization for this
+      actionTypes: {
+        [ActionType.ACTIVE]: 'anima.ui.psychic.psychicPowers.actionType.active.title',
+        [ActionType.PASSIVE]: 'anima.ui.psychic.psychicPowers.actionType.passive.title'
+      },
+      //TODO: Create new localization for this
+      resistances: {
+        [ResistanceType.PHYSICAL]: 'anima.ui.resistances.physical',
+        [ResistanceType.DISEASE]: 'anima.ui.resistances.disease',
+        [ResistanceType.POISON]: 'anima.ui.resistances.poison',
+        [ResistanceType.MAGIC]: 'anima.ui.resistances.magic',
+        [ResistanceType.PSYCHIC]: 'anima.ui.resistances.psychic',
+        [ResistanceType.NONE]: 'anima.ui.resistances.none'
+      },
       weapon: {
+        equippedHandTypes: {
+          [WeaponEquippedHandType.ONE_HANDED]: '',
+          [WeaponEquippedHandType.TWO_HANDED]: ''
+        },
+        knowledgeTypes: {
+          [WeaponKnowledgeType.KNOWN]: '',
+          [WeaponKnowledgeType.SIMILAR]: '',
+          [WeaponKnowledgeType.MIXED]: '',
+          [WeaponKnowledgeType.DIFFERENT]: ''
+        },
         sizes: {
           [WeaponSize.SMALL]: 'anima.ui.combat.weapon.size.small.title',
           [WeaponSize.MEDIUM]: 'anima.ui.combat.weapon.size.medium.title',
@@ -60,100 +90,99 @@ export const ABFConfig = /** @type {const} */ ({
     },
     mystic: {
       vias: {
-        air: 'anima.ui.mystic.spell.via.air.title',
-        blood: 'anima.ui.mystic.spell.via.blood.title',
-        chaos: 'anima.ui.mystic.spell.via.chaos.title',
-        creation: 'anima.ui.mystic.spell.via.creation.title',
-        darkness: 'anima.ui.mystic.spell.via.darkness.title',
-        death: 'anima.ui.mystic.spell.via.death.title',
-        destruction: 'anima.ui.mystic.spell.via.destruction.title',
-        dreams: 'anima.ui.mystic.spell.via.dreams.title',
-        earth: 'anima.ui.mystic.spell.via.earth.title',
-        emptiness: 'anima.ui.mystic.spell.via.emptiness.title',
-        essence: 'anima.ui.mystic.spell.via.essence.title',
-        fire: 'anima.ui.mystic.spell.via.fire.title',
-        freeAccess: 'anima.ui.mystic.spell.via.freeAccess.title',
-        illusion: 'anima.ui.mystic.spell.via.illusion.title',
-        knowledge: 'anima.ui.mystic.spell.via.knowledge.title',
-        light: 'anima.ui.mystic.spell.via.light.title',
-        literae: 'anima.ui.mystic.spell.via.literae.title',
-        musical: 'anima.ui.mystic.spell.via.musical.title',
-        necromancy: 'anima.ui.mystic.spell.via.necromancy.title',
-        nobility: 'anima.ui.mystic.spell.via.nobility.title',
-        peace: 'anima.ui.mystic.spell.via.peace.title',
-        sin: 'anima.ui.mystic.spell.via.sin.title',
-        threshold: 'anima.ui.mystic.spell.via.threshold.title',
-        time: 'anima.ui.mystic.spell.via.time.title',
-        war: 'anima.ui.mystic.spell.via.war.title',
-        water: 'anima.ui.mystic.spell.via.water.title'
+        [Via.AIR]: 'anima.ui.mystic.spell.via.air.title',
+        [Via.BLOOD]: 'anima.ui.mystic.spell.via.blood.title',
+        [Via.CHAOS]: 'anima.ui.mystic.spell.via.chaos.title',
+        [Via.CREATION]: 'anima.ui.mystic.spell.via.creation.title',
+        [Via.DARKNESS]: 'anima.ui.mystic.spell.via.darkness.title',
+        [Via.DEATH]: 'anima.ui.mystic.spell.via.death.title',
+        [Via.DESTRUCTION]: 'anima.ui.mystic.spell.via.destruction.title',
+        [Via.DREAMS]: 'anima.ui.mystic.spell.via.dreams.title',
+        [Via.EARTH]: 'anima.ui.mystic.spell.via.earth.title',
+        [Via.EMPTINESS]: 'anima.ui.mystic.spell.via.emptiness.title',
+        [Via.ESSENCE]: 'anima.ui.mystic.spell.via.essence.title',
+        [Via.FIRE]: 'anima.ui.mystic.spell.via.fire.title',
+        [Via.FREEACCESS]: 'anima.ui.mystic.spell.via.freeAccess.title',
+        [Via.ILLUSION]: 'anima.ui.mystic.spell.via.illusion.title',
+        [Via.KNOWLEDGE]: 'anima.ui.mystic.spell.via.knowledge.title',
+        [Via.LIGHT]: 'anima.ui.mystic.spell.via.light.title',
+        [Via.LITERAE]: 'anima.ui.mystic.spell.via.literae.title',
+        [Via.MUSICAL]: 'anima.ui.mystic.spell.via.musical.title',
+        [Via.NECROMANCY]: 'anima.ui.mystic.spell.via.necromancy.title',
+        [Via.NOBILITY]: 'anima.ui.mystic.spell.via.nobility.title',
+        [Via.PEACE]: 'anima.ui.mystic.spell.via.peace.title',
+        [Via.SIN]: 'anima.ui.mystic.spell.via.sin.title',
+        [Via.THRESHOLD]: 'anima.ui.mystic.spell.via.threshold.title',
+        [Via.TIME]: 'anima.ui.mystic.spell.via.time.title',
+        [Via.WAR]: 'anima.ui.mystic.spell.via.war.title',
+        [Via.WATER]: 'anima.ui.mystic.spell.via.water.title'
       },
+      //TODO: Delete this after changing all occurrences
       actionTypes: {
-        active: 'anima.ui.mystic.spell.actionType.active.title',
-        passive: 'anima.ui.mystic.spell.actionType.passive.title'
+        [ActionType.ACTIVE]: 'anima.ui.mystic.spell.actionType.active.title',
+        [ActionType.PASSIVE]: 'anima.ui.mystic.spell.actionType.passive.title'
       },
+      //TODO: Delete this after changing all occurrences
       combatTypes: {
-        attack: 'anima.ui.mystic.spell.combatType.attack.title',
-        defense: 'anima.ui.mystic.spell.combatType.defense.title',
-        none: 'anima.ui.mystic.spell.combatType.none.title'
+        [CombatType.ATTACK]: 'anima.ui.mystic.spell.combatType.attack.title',
+        [CombatType.DEFENSE]: 'anima.ui.mystic.spell.combatType.defense.title',
+        [CombatType.NONE]: 'anima.ui.mystic.spell.combatType.none.title'
       },
       spellTypes: {
-        attack: 'anima.ui.mystic.spell.spellType.attack.title',
-        defense: 'anima.ui.mystic.spell.spellType.defense.title',
-        animatic: 'anima.ui.mystic.spell.spellType.animatic.title',
-        effect: 'anima.ui.mystic.spell.spellType.effect.title',
-        automatic: 'anima.ui.mystic.spell.spellType.automatic.title',
-        detection: 'anima.ui.mystic.spell.spellType.detection.title'
+        [SpellType.ATTACK]: 'anima.ui.mystic.spell.spellType.attack.title',
+        [SpellType.DEFENSE]: 'anima.ui.mystic.spell.spellType.defense.title',
+        [SpellType.ANIMATIC]: 'anima.ui.mystic.spell.spellType.animatic.title',
+        [SpellType.EFFECT]: 'anima.ui.mystic.spell.spellType.effect.title',
+        [SpellType.AUTOMATIC]: 'anima.ui.mystic.spell.spellType.automatic.title',
+        [SpellType.DETECTION]: 'anima.ui.mystic.spell.spellType.detection.title'
       },
       spellGrades: {
-        [SpellGrades.BASE]: 'anima.ui.mystic.spell.grade.base.title',
-        [SpellGrades.INTERMEDIATE]: 'anima.ui.mystic.spell.grade.intermediate.title',
-        [SpellGrades.ADVANCED]: 'anima.ui.mystic.spell.grade.advanced.title',
-        [SpellGrades.ARCANE]: 'anima.ui.mystic.spell.grade.arcane.title'
+        [SpellGrade.BASE]: 'anima.ui.mystic.spell.grade.base.title',
+        [SpellGrade.INTERMEDIATE]: 'anima.ui.mystic.spell.grade.intermediate.title',
+        [SpellGrade.ADVANCED]: 'anima.ui.mystic.spell.grade.advanced.title',
+        [SpellGrade.ARCANE]: 'anima.ui.mystic.spell.grade.arcane.title'
       }
     },
     psychic: {
+      //TODO: Delete this after changing all occurrences
       actionTypes: {
-        [PsychicPowerActionTypes.ACTIVE]:
-          'anima.ui.psychic.psychicPowers.actionType.active.title',
-        [PsychicPowerActionTypes.PASSIVE]:
-          'anima.ui.psychic.psychicPowers.actionType.passive.title'
+        [ActionType.ACTIVE]: 'anima.ui.psychic.psychicPowers.actionType.active.title',
+        [ActionType.PASSIVE]: 'anima.ui.psychic.psychicPowers.actionType.passive.title'
       },
+      //TODO: Delete this after changing all occurrences
       combatTypes: {
-        [PsychicPowerCombatTypes.ATTACK]:
-          'anima.ui.psychic.psychicPowers.combatType.attack.title',
-        [PsychicPowerCombatTypes.DEFENSE]:
-          'anima.ui.psychic.psychicPowers.combatType.defense.title',
-        [PsychicPowerCombatTypes.NONE]:
-          'anima.ui.psychic.psychicPowers.combatType.none.title'
+        [CombatType.ATTACK]: 'anima.ui.psychic.psychicPowers.combatType.attack.title',
+        [CombatType.DEFENSE]: 'anima.ui.psychic.psychicPowers.combatType.defense.title',
+        [CombatType.NONE]: 'anima.ui.psychic.psychicPowers.combatType.none.title'
       },
       disciplines: {
-        [PsychicPowerDisciplines.MATRIX_POWERS]:
+        [PsychicDiscipline.MATRIX_POWERS]:
           'anima.ui.psychic.psychicPowers.discipline.matrixPowers.title',
-        [PsychicPowerDisciplines.TELEPATHY]:
+        [PsychicDiscipline.TELEPATHY]:
           'anima.ui.psychic.psychicPowers.discipline.telepathy.title',
-        [PsychicPowerDisciplines.TELEKINESIS]:
+        [PsychicDiscipline.TELEKINESIS]:
           'anima.ui.psychic.psychicPowers.discipline.telekenisis.title',
-        [PsychicPowerDisciplines.PYROKINESIS]:
+        [PsychicDiscipline.PYROKINESIS]:
           'anima.ui.psychic.psychicPowers.discipline.pyrokinesis.title',
-        [PsychicPowerDisciplines.CRYOKINESIS]:
+        [PsychicDiscipline.CRYOKINESIS]:
           'anima.ui.psychic.psychicPowers.discipline.cryokinesis.title',
-        [PsychicPowerDisciplines.PHYSICAL_INCREASE]:
+        [PsychicDiscipline.PHYSICAL_INCREASE]:
           'anima.ui.psychic.psychicPowers.discipline.physicalIncrease.title',
-        [PsychicPowerDisciplines.ENERGY]:
+        [PsychicDiscipline.ENERGY]:
           'anima.ui.psychic.psychicPowers.discipline.energy.title',
-        [PsychicPowerDisciplines.TELEMETRY]:
+        [PsychicDiscipline.TELEMETRY]:
           'anima.ui.psychic.psychicPowers.discipline.telemetry.title',
-        [PsychicPowerDisciplines.SENTIENT]:
+        [PsychicDiscipline.SENTIENT]:
           'anima.ui.psychic.psychicPowers.discipline.sentient.title',
-        [PsychicPowerDisciplines.CAUSALITY]:
+        [PsychicDiscipline.CAUSALITY]:
           'anima.ui.psychic.psychicPowers.discipline.causality.title',
-        [PsychicPowerDisciplines.ELECTROMAGNETISM]:
+        [PsychicDiscipline.ELECTROMAGNETISM]:
           'anima.ui.psychic.psychicPowers.discipline.electromagnetism.title',
-        [PsychicPowerDisciplines.TELEPORTATION]:
+        [PsychicDiscipline.TELEPORTATION]:
           'anima.ui.psychic.psychicPowers.discipline.teleportation.title',
-        [PsychicPowerDisciplines.LIGHT]:
+        [PsychicDiscipline.LIGHT]:
           'anima.ui.psychic.psychicPowers.discipline.light.title',
-        [PsychicPowerDisciplines.HYPERSENSITIVITY]:
+        [PsychicDiscipline.HYPERSENSITIVITY]:
           'anima.ui.psychic.psychicPowers.discipline.hypersensitivity.title'
       }
     }
