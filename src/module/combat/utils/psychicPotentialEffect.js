eac055c6150b081a4325391e779039bc83dca309
JulenBZ <julen@bravezebra.es>
2026-02-12 11:33:02 +0100

    FIX - Psychic potential is no loger liited by inhumanity/zen

--- src/module/combat/utils/psychicPotentialEffect.js
+++ src/module/combat/utils/psychicPotentialEffect.js
@@ -1,6 +1,6 @@
 /**
  * Calculates the effect of psychic potential based on different factors.
- * 
+ *
  * @param {number} potential - The psychic potential value.
  * @param {number} imbalance - The imbalance factor.
  * @param {boolean} inhuman - Indicates if the psychic is inhuman.
@@ -9,15 +9,27 @@
  */
 export const difficultyRange = [20, 40, 80, 120, 140, 180, 240, 280, 320, 440];
 export const psychicPotentialEffect = (potential, imbalance, inhuman, zen) => {
-    let max = zen ? 9 : inhuman ? 8 : 7;
-    if (potential < 40) { return difficultyRange[0 + imbalance] }
-    else if (potential < 80) { return difficultyRange[1 + imbalance] }
-    else if (potential < 120) { return difficultyRange[2 + imbalance] }
-    else if (potential < 140) { return difficultyRange[3 + imbalance] }
-    else if (potential < 180) { return difficultyRange[4 + imbalance] }
-    else if (potential < 240) { return difficultyRange[5 + imbalance] }
-    else if (potential < 280) { return difficultyRange[6 + imbalance] }
-    else if (potential < 320) { return difficultyRange[Math.min(max, 7 + imbalance)] }
-    else if (potential < 440) { return difficultyRange[Math.min(max, 8 + imbalance)] }
-    else { return difficultyRange[Math.min(max, 9)] }
-}
+  // let max = zen ? 9 : inhuman ? 8 : 7;
+  let max = 9;
+  if (potential < 40) {
+    return difficultyRange[0 + imbalance];
+  } else if (potential < 80) {
+    return difficultyRange[1 + imbalance];
+  } else if (potential < 120) {
+    return difficultyRange[2 + imbalance];
+  } else if (potential < 140) {
+    return difficultyRange[3 + imbalance];
+  } else if (potential < 180) {
+    return difficultyRange[4 + imbalance];
+  } else if (potential < 240) {
+    return difficultyRange[5 + imbalance];
+  } else if (potential < 280) {
+    return difficultyRange[6 + imbalance];
+  } else if (potential < 320) {
+    return difficultyRange[Math.min(max, 7 + imbalance)];
+  } else if (potential < 440) {
+    return difficultyRange[Math.min(max, 8 + imbalance)];
+  } else {
+    return difficultyRange[Math.min(max, 9)];
+  }
+};
